8dd663df4c5408b118dd809129484935677273bf
danielssonSImon <simon.danielsson6@gmail.com>
2018-11-11 16:55:42 +0100

    initial comit

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 require_once('connect.php');
 $conn=connect();
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<link rel="stylesheet" href="css.css">
+  <head>
+    <meta charset="utf-8">
+    <title>Snabbkontakt</title>
+  </head>
+  <body>
+
+  </body>
+</html>

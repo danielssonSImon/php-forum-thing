b899e504b41f5d4626501a6ec96bbdc1ff5b20a8
danielssonSImon <simon.danielsson6@gmail.com>
2018-12-14 13:09:01 +0100

    fungerar igen

--- index.php
+++ index.php
@@ -3,13 +3,30 @@ require_once('connect.php');
 $conn=connect();
  ?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-<link rel="stylesheet" href="css.css">
+<html lang="en">
+<link rel="stylesheet" href="../css.css">
   <head>
     <meta charset="utf-8">
-    <title>Snabbkontakt</title>
+    <title>Ask a question!</title>
   </head>
   <body>
+    <div class="content">
+      <div class="header">
 
+      </div>
+      <div class="main">
+
+      </div>
+      <div class="text">
+
+      </div>
+        <form class="form" action="submit.php" method="post">
+            <input class="input" type="text" name="text" value="WHat ">
+            <input class="submit"type="submit" name="submit" value="Submit">
+        </form>
+      <div class="main2">
+
+      </div>
+    </div>
   </body>
 </html>
